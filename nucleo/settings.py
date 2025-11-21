8af43d5f2f52feb9e51a58072a6f2b1e88ad602d
cyberdexter82 <alejandrocazango@gmail.com>
2025-11-20 20:12:52 -0600

    Fix: Removed unsupported argument conn_health_check from DB config

--- nucleo/settings.py
+++ nucleo/settings.py
@@ -69,7 +69,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, conn_health_check=True)
+'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
 else:
     DATABASES = {
